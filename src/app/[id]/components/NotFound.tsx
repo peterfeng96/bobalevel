16f474bc40d9c2e3e5773eee90c8662cee6f7507
Peter <peterfeng96@gmail.com>
2023-11-09 21:12:25 -0600

    notfound page

---
+++ src/app/[id]/components/NotFound.tsx
@@ -0,0 +1,17 @@
+import { Box, Typography } from "@mui/material";
+
+export default function NotFound() {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100%",
+      }}
+    >
+      <Typography>You might be lost</Typography>
+    </Box>
+  );
+}
--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,45 +1,52 @@
 //PAGE FOR INDIVIDUAL USERS
 //Module imports
 import { Container, Typography } from "@mui/material";
-import { useRouter } from "next/navigation";
+import { Link } from "@mui/material";
+import Image from "next/image";
 //Import Context and Components
+import NotFound from "./components/NotFound";
 import SettingsSection from "./components/SettingsSection";
 import PostsSection from "./components/PostsSection";
 import { getUserData } from "@/utils/utils";
-import { Link } from "@mui/material";
-import Image from "next/image";
 
 export default async function Page({ params }: { params: { id: string } }) {
   //Fetching user data from backend
   const userData: any = await getUserData(params.id);
-  const router = useRouter();
-  if (!userData) router.push("/login");
 
   return (
     <main style={{ margin: "5vh 0" }}>
-      <Container
-        maxWidth="sm"
-        style={{ display: "flex", flexDirection: "column", gap: 40 }}
-      >
-        <SettingsSection settings={userData.settings} />
-        <PostsSection posts={userData.posts} />
-        <Link
-          sx={{
-            display: "flex",
-            flexGrow: 1,
-            justifyContent: "center",
-            alignItems: "flex-end",
-            gap: "1vw",
-            textDecoration: "none",
-          }}
-          href="https://bobalevel.com"
+      {userData ? (
+        <Container
+          maxWidth="sm"
+          style={{ display: "flex", flexDirection: "column", gap: 40 }}
         >
-          <Image src="/boba.png" alt="Boba Icon" width={45} height={45}></Image>
-          <Typography variant="h2" color="textSecondary">
-            Boba-Level
-          </Typography>
-        </Link>
-      </Container>
+          <SettingsSection settings={userData.settings} />
+          <PostsSection posts={userData.posts} />
+          <Link
+            sx={{
+              display: "flex",
+              flexGrow: 1,
+              justifyContent: "center",
+              alignItems: "flex-end",
+              gap: "1vw",
+              textDecoration: "none",
+            }}
+            href="https://bobalevel.com"
+          >
+            <Image
+              src="/boba.png"
+              alt="Boba Icon"
+              width={45}
+              height={45}
+            ></Image>
+            <Typography variant="h2" color="textSecondary">
+              Boba-Level
+            </Typography>
+          </Link>
+        </Container>
+      ) : (
+        <NotFound />
+      )}
     </main>
   );
 }
